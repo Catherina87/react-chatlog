16946d4406037554f3fdcbf7ebd1fd7e966c9236
Catherina <catherine.pchelina@gmail.com>
2020-04-14 14:59:56 -0700

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatLog from './components/ChatLog';
 
 const App = () => {
   console.log(chatMessages);
@@ -8,10 +9,10 @@ const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir an Estragon</h1>
       </header>
       <main>
-
+        <ChatLog messages={chatMessages}/>
       </main>
     </div>
   );
